187b2690acef83d4b3ab61bd6f49f6056287cc21
Matthew Palermo <matt.r.palermo@gmail.com>
2016-12-24 03:38:49 +1000

    Refine stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,15 @@ module.exports = {
 	"extends": "stylelint-config-standard",
 	"rules": {
 		"indentation": "tab",
+		"font-family-name-quotes": "always-unless-keyword",
+		//"declaration-block-properties-order": ...
+		// Since we're using autoprefixer
+		"at-rule-no-vendor-prefix": true,
+		"media-feature-name-no-vendor-prefix": true,
+		"property-no-vendor-prefix": true,
+		"selector-no-vendor-prefix": true,
+		"value-no-vendor-prefix": true,
+		"no-browser-hacks": true,
+		"no-unsupported-browser-features": true
 	}
 };
