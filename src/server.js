22373b8da1ac972b9620807825a2e79c48ecc9c1
Matthew Palermo <matt.r.palermo@gmail.com>
2016-12-24 01:01:48 +1000

    Remove old code

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-//app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/', function(req, res){
   //hello
